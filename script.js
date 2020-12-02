a2d6544aff2fd3f8fe37e3c8cc84d271a08a4855
Mona Lisa <clarkefraser1@gmail.com>
2020-12-02 15:04:37 +1100

    psudo-ing js file

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
-                                                                                       // Assignment Code
+
+
+//Pseudo
+//When button clicked prompt user for criteria
+//Prompt length min 8 max 128
+//Prompt Character types, choose lowercase,
+//uppercase,numeric, and or special characters.
+//Answers input shoulb be validated, at least
+//one character type be selected.
+//Once generated display on page.
+
+
+//prompt function
+
+//variables
+//length
+//lower/upper/numbers,charcters
+
+//some logic and function wizardry.
+
+
+
+
+
+
+// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
