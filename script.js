cd99b685b3164cbbaee6fb7d3c1dd2ba85ad0e17
Mona Lisa <clarkefraser1@gmail.com>
2020-12-02 15:51:35 +1100

    pseudo-ing js file

--- script.js
+++ script.js
@@ -19,7 +19,16 @@
 //some logic and function wizardry.
 
 
+var length = ("8-128")
+var lowercase = ("q,w,e,r,t,y,u,i,o,p,a,s,d,f,g,h,j,k,l,z,x,c,v,b,n,m");
+var uppercase = ("Q,W,E,R,T,Y,U,I,O,P,A,S,D,F,G,H,J,K,L,Z,X,C,V,B,N,M");
+var numbers = ("1,2,3,4,5,6,7,8,9,0");
+//Unicode????? for characters
+var characters =
 
+//confirm funtion??
+
+//mathfloor mathrandom function
 
 
 
