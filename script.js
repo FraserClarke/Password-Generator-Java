7c418680a7b7be5b0319e835d6c733a2b3f108c4
Mona Lisa <clarkefraser1@gmail.com>
2020-12-15 13:52:31 +1100

    adding inputs for html elements

--- script.js
+++ script.js
@@ -19,18 +19,23 @@
 //some logic and function wizardry.
 
 
-var length = ("8-128")
-var lowercase = ("q,w,e,r,t,y,u,i,o,p,a,s,d,f,g,h,j,k,l,z,x,c,v,b,n,m");
-var uppercase = ("Q,W,E,R,T,Y,U,I,O,P,A,S,D,F,G,H,J,K,L,Z,X,C,V,B,N,M");
-var numbers = ("1,2,3,4,5,6,7,8,9,0");
+var lengthEl = ("8-128")
+var lowercaseEl = ("q,w,e,r,t,y,u,i,o,p,a,s,d,f,g,h,j,k,l,z,x,c,v,b,n,m");
+var uppercaseEl = ("Q,W,E,R,T,Y,U,I,O,P,A,S,D,F,G,H,J,K,L,Z,X,C,V,B,N,M");
+var numbersEl = ("1,2,3,4,5,6,7,8,9,0");
 //Unicode????? for characters
-var characters =
+
 
 //confirm funtion??
 //return function
 
 //mathfloor mathrandom function
 //length fuction
+prompt("Choose passoword length, min 8, max 128");
+// for (var i = 0; i < langthEl.length; i++) {
+
+}
+
 
 
 
